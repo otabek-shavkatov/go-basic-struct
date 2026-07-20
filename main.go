d041c858a58bbebf80a5e965ac7de55212e7b813
Otabek Shavkatov <otabekshavkatov839@gmail.com>
2026-07-20 21:02:21 +0500

    started new homework lesson 10

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
-import "fmt"
+// type FeaturesStruct struct {
+// 	Color   [3]string
+// 	weight  int
+// 	height  int
+// 	opacity float64
+// 	status  bool
+// }
 
-type FeaturesStruct struct {
-	Color   [3]string
-	weight  int
-	height  int
-	opacity float64
-	status  bool
-}
+// type UserList struct {
+// 	name string
+// }
 
 func main() {
 
@@ -27,23 +29,23 @@ func main() {
 	// 	"Fruits": products,
 	// }
 
-	featuresMap := map[string]string{
-		"Color":   "Red",
-		"weight":  "100",
-		"height":  "200",
-		"opacity": "0.99",
-	}
+	// featuresMap := map[string]string{
+	// 	"Color":   "Red",
+	// 	"weight":  "100",
+	// 	"height":  "200",
+	// 	"opacity": "0.99",
+	// }
 
-	featuresStruct := FeaturesStruct{
-		Color:   [3]string{"Red", "Green", "Blue"},
-		weight:  100,
-		height:  200,
-		opacity: 0.99,
-		status:  true,
-	}
+	// featuresStruct := FeaturesStruct{
+	// 	Color:   [3]string{"Red", "Green", "Blue"},
+	// 	weight:  100,
+	// 	height:  200,
+	// 	opacity: 0.99,
+	// 	status:  true,
+	// }
 
-	fmt.Println(featuresMap)
-	fmt.Printf("Features Struct: %+v\n", featuresStruct)
+	// fmt.Println(featuresMap)
+	// fmt.Printf("Features Struct: %+v\n", featuresStruct)
 
 	// fmt.Println("Category:")
 	// fmt.Println(category)
@@ -53,22 +55,30 @@ func main() {
 
 	// map and struct
 
-	market := map[string]FeaturesStruct{
-		"Apple": {
-			Color:   [3]string{"Red", "Green", "Blue"},
-			weight:  100,
-			height:  200,
-			opacity: 0.99,
-			status:  true,
-		},
-		"Banana": {
-			Color:   [3]string{"Yellow", "Green", "Brown"},
-			weight:  150,
-			height:  250,
-			opacity: 0.95,
-			status:  true,
-		},
-	}
+	// market := map[string]FeaturesStruct{
+	// 	"Apple": {
+	// 		Color:   [3]string{"Red", "Green", "Blue"},
+	// 		weight:  100,
+	// 		height:  200,
+	// 		opacity: 0.99,
+	// 		status:  true,
+	// 	},
+	// 	"Banana": {
+	// 		Color:   [3]string{"Yellow", "Green", "Brown"},
+	// 		weight:  150,
+	// 		height:  250,
+	// 		opacity: 0.95,
+	// 		status:  true,
+	// 	},
+	// }
 
-	fmt.Printf("Market: %+v\n", market)
+	// setName := UserList{name: "Javlonbek"}
+	// setName.changeName("Otabek")
+	// fmt.Println(setName.name)
+	// fmt.Printf("Market: %+v\n", market)
 }
+
+// func (u UserList) changeName(name string) []string {
+// 	u.name = name
+// 	return []string{name}
+// }
